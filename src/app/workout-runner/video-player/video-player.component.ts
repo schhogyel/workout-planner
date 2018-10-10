0a1a46213ea1b04ded019c6279a1ba005c6ab7a3
Sonam Chhogyel <sonam.chhogyel@empired.com>
2018-10-10 14:17:13 +1000

    add more feature components

---
+++ src/app/workout-runner/video-player/video-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'abe-video-player',
+  templateUrl: './video-player.component.html',
+  styles: []
+})
+export class VideoPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/workout-runner/workout-runner.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { WorkoutRunnerComponent } from "./workout-runner.component";
+import { ExerciseDescriptionComponent } from './exercise-description/exercise-description.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [WorkoutRunnerComponent],
+  declarations: [WorkoutRunnerComponent, ExerciseDescriptionComponent, VideoPlayerComponent]
+})
+export class WorkoutRunnerModule {}
